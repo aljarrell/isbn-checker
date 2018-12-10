619a2f1b15293580805b6f318831ed1b1f7e2c77
aljarrell <alan.k.jarrell@gmail.com>
2018-12-10 15:18:54 -0500

    did an upcase on my check ten function so it accounts for capital x's as well

--- isbn.rb
+++ isbn.rb
@@ -1,9 +1,9 @@
 def check_ten(number)
-  number = number.to_s.tr("a-w", "").tr("y-z", "").gsub("-", "").gsub(" ", "").each_char.to_a #removes hyphens, spaces, and tr letters a-w and y-z to empty strings so that x can still pass through as a check number
+  number = number.to_s.upcase.tr("a-w", "").tr("y-z", "").gsub("-", "").gsub(" ", "").each_char.to_a #removes hyphens, spaces, and tr letters a-w and y-z to empty strings so that x can still pass through as a check number
     #p "this is NUMBER #{number}"
   if number.length == 10
     check_number = number.pop #returns last number in the array
-    check_number = check_number.gsub("x", "10").to_i
+    check_number = check_number.gsub("X", "10").to_i
     #p "this is CHECK NUMBER #{check_number}"
     total = 0
     number.each_with_index do |num, index|
