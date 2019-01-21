9dd684fb54b4568513c761fb8c9390844ed49da5
aljarrell <alan.k.jarrell@gmail.com>
2019-01-21 11:26:44 -0500

    reading and writing to bucket properly

--- app.rb
+++ app.rb
@@ -6,6 +6,8 @@ require_relative 'isbn.rb'
 require_relative 'csv.rb'
 require_relative 'bucket_functions.rb'
 
+load './local_ENV.rb' if File.exist?('./local_ENV.rb')
+
 get '/' do
   erb :home
 end
@@ -14,20 +16,21 @@ post '/home' do
   isbn = params[:isbn]
   csv_file = params[:csv_file]
   read = update_csv(csv_file).join(", ")
-  redirect 'results?isbn=' + isbn + '&read=' + read
+  redirect 'results?isbn=' + isbn + '&read=' + read + '&csv_file=' + csv_file
 end
 
 get '/results' do
-  bucket_function = bucket_stuff()
+  csv_file = params[:csv_file]
   isbn = params[:isbn]
   read = params[:read]
   isbn = isbn.upcase.tr("a-w", "").tr("y-z", "").tr('"', '').gsub("-", "").gsub(" ", "").each_char.to_a
-  #p "CSV FILE #{csv_file}"
   if isbn.length == 10
     result = check_ten(isbn.join(""))
   else
     result = check_thirteen(isbn.join(""))
   end
-
-  erb :results, locals: {isbn: isbn, result: result, read: read}
+  write_new_isbn_to_bucket(isbn.join)
+  write_to_bucket(csv_file)
+  bucket_function = read_bucket()
+  erb :results, locals: {isbn: isbn, result: result, read: read, bucket_function: bucket_function, csv_file: csv_file}
 end
--- bucket_functions.rb
+++ bucket_functions.rb
@@ -1,10 +1,9 @@
 require 'aws-sdk'
 require 'csv'
+require_relative 'isbn.rb'
 
-load './local_ENV.rb' if File.exist?('./local_ENV.rb')
 
-
-def bucket_stuff()
+def read_bucket()
     s3 = Aws::S3::Client.new(
                 access_key_id: ENV['S3_KEY'],
                 secret_access_key: ENV['S3_SECRET'],
@@ -12,11 +11,60 @@ def bucket_stuff()
                 )
     bucket = ENV['S3_BUCKET']
     file = ENV['S3_File']  
+    resp = s3.get_object(bucket: bucket, key: file)
+    read_arr = Array.new
+    CSV.parse(resp.body) do |row|
+        p "THIS IS ROW #{row}"
+        number = row[1].to_s.tr("a-w", "").tr("y-z", "").gsub("-", "").gsub(" ", "").gsub("\"", "").gsub("\]", "").gsub("/[", "").gsub(",", "") 
+            if number.length == 10
+                read_arr.push([number + " is " + check_ten(number)])
+            else
+                read_arr.push([number + " is " + check_thirteen(number)])
+            end 
+    end 
+    p "THIS IS READ_ARR #{read_arr}"
+    read_arr
+end 
+
+def write_to_bucket(file)
+    s3 = Aws::S3::Client.new(
+                access_key_id: ENV['S3_KEY'],
+                secret_access_key: ENV['S3_SECRET'],
+                region: ENV['S3_Region'],
+                )
+    bucket = ENV['S3_BUCKET']  
     resp = s3.get_object(bucket: bucket, key: ENV['S3_File'])
-    validlist = Array.new
-    isbnlist = Array.new 
-    multiarray = CSV.parse(resp.body)
-    multiarray.each do |row|
-        #p "this is ROW #{row}"
-    end
+    s3 = Aws::S3::Resource.new(
+                access_key_id: ENV['S3_KEY'],
+                secret_access_key: ENV['S3_SECRET'],
+                region: ENV['S3_Region'],
+                )
+    CSV.read(file, "r") do |row|
+        CSV.open(resp.body, "wb") do |csv|
+            csv << row 
+        end 
+    end 
+    obj = s3.bucket(bucket).object(ENV['S3_File'])
+    obj.upload_file(file)
+end 
+
+def write_new_isbn_to_bucket(isbn)
+    s3 = Aws::S3::Client.new(
+                access_key_id: ENV['S3_KEY'],
+                secret_access_key: ENV['S3_SECRET'],
+                region: ENV['S3_Region'],
+                )
+    bucket = ENV['S3_BUCKET']
+    file = ENV['S3_File']  
+    resp = s3.get_object(bucket: bucket, key: file)
+    s3 = Aws::S3::Resource.new(
+                access_key_id: ENV['S3_KEY'],
+                secret_access_key: ENV['S3_SECRET'],
+                region: ENV['S3_Region'],
+                )
+    File.open(file, "a+") do |csv|
+        csv << "\n" + "placeholder, " + isbn
+    end 
+    obj = s3.bucket(bucket).object(ENV['S3_File'])
+    obj.upload_file(ENV['S3_File'])
 end 
